a8a5dd0c65d15cf87e669525cc431f1ec0e7b0b5
Vaibhav-Singh2110 <vaibhavmsd39@gmail.com>
2023-05-31 19:32:40 +0530

    up

--- server.js
+++ server.js
@@ -11,17 +11,20 @@ const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
     return res.send('Hello World');
+    return res.send('');
 });
 
 baseRouter.post('/add', (req, res) => {
     const {first,second} = req.body
     res.json({ "result": first+second });
+    res.json({ "": null });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
     const {first,second} = req.body
     res.json({ "result": first-second });
+    res.json({ "": null });
 });
 
 app.use(baseUrl, baseRouter);
